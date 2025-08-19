52635d6715f7d553914a354e623f1073ba944d1f
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-08-19 23:42:29 +0330

    feat(ui): add navigation function and update UI in navbar component

--- src/components/modules/navbar/navbar.jsx
+++ src/components/modules/navbar/navbar.jsx
@@ -1,54 +1,86 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-import { IoCloseOutline } from "react-icons/io5";
-import { GoArrowRight } from "react-icons/go";
-import { LuTicketPercent } from "react-icons/lu";
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
 
-  const [showBanner, setShowBanner] = useState(true);
+  const [menuItems, setMenuItems] = useState([
+    { name: 'Home', path: '/' },
+    { name: 'Shop', path: '/Shop' },
+    { name: 'Product', path: '/Product' },
+    { name: 'Contact us', path: '/contact-us' },
+  ]);
+  const location = useLocation();
+
+  // تابع بررسی فعال بودن مسیر
+  const isActive = (path) => {
+    if (!path) return false;
+    if (path === '/') return location.pathname === '/';
+    return location.pathname.startsWith(path);
+  };
+
+  // تابع بررسی فعال بودن منو
+  const isMenuActive = (menu) => {
+    if (isActive(menu.path)) return true;
+  };
 
   return (
-    <>
-      {showBanner && (
-        <header className="relative flex items-center justify-end w-full h-10 bg-neutral-02">
-          <div className="inline-flex items-center justify-center gap-3 absolute top-2 left-0 right-0">
-            <div className="inline-flex items-center justify-center gap-3 relative">
-              <LuTicketPercent
-              className="relative size-6 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
-              />
-              <p className="font-InterSemiBold text-neutral-05 leading-[22px] text-center">
-                30% off storewide — Limited time!
-              </p>
-            </div>
-            <button
-              className="inline-flex items-center gap-0.5 border-b border-secondary-color-blue"
-              aria-label="Shop now for 30% off"
-            >
-              <div className="inline-flex items-center text-secondary-color-blue gap-1">
+    <div>
+      <div className="container">
+        <nav className="w-full bg-white flex items-center justify-between">
+
+          <h1 className="font-PoppinsMedium text-2xl leading-none py-4.5">
+            <span className="text-black hover:drop-shadow-custom transition-all">3legant</span>
+            <span className="text-neutral-04">.</span>
+          </h1>
+
+          <ul className="flex items-center gap-10 py-4.5 font-InterMedium text-sm">
+            {menuItems.map((menu) => (
+              <li
+                className='group'
+                key={crypto.randomUUID()}>
                 <Link
-                to="/Shop"
-                className="font-button-XS text-blue">
-                  Shop Now
+                  to={menu.path}
+                  className={`group-hover:drop-shadow-custom transition-all ${isMenuActive(menu) ? 'text-neutral-07' : 'text-neutral-04'}`}>
+                  {menu.name}
                 </Link>
-                <GoArrowRight
-                  className="relative size-4.5"
-                />
-              </div>
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex items-center gap-4 py-4">
+            <button>
+              <img
+                src='/Images/Search.svg'
+                alt='search-icon'
+                className="size-6 hover:drop-shadow-custom transition-all" />
             </button>
+            <button>
+              <img
+                src='/Images/User.svg'
+                alt='user-icon'
+                className="size-6 hover:drop-shadow-custom transition-all" />
+            </button>
+            <div className='flex items-center justify-center gap-2'>
+              <Link
+                to="/Shop"
+                className="relative">
+                <img
+                  src='/Images/Shop.svg'
+                  alt='shop-icon'
+                  className="size-6 hover:drop-shadow-custom transition-all" />
+                <img
+                  src="/Images/Curve.svg"
+                  alt="shop-icon2"
+                  className='absolute top-1 right-2 size-2' />
+              </Link>
+              <span className="flex items-center justify-center font-InterBold leading-0.83 bg-neutral-07 text-white text-xs rounded-full size-5.5">
+                2
+              </span>
+            </div>
           </div>
-          <button
-            
-            aria-label="Close banner"
-            onClick={() => setShowBanner(false)}
-          >
-            <IoCloseOutline
-              className="absolute size-5 top-2 right-10 cursor-pointer"
-            />
-          </button>
-        </header>
-      )}
-    </>
+        </nav>
+      </div>
+    </div>
   )
 }
 
