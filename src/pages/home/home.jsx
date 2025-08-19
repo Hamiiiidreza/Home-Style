7b226c786018f745977451e78b7304356c102f98
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-08-19 23:55:31 +0330

    feat: import header in home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import Navbar from '../../components/modules/navbar/navbar'
+import Topbar from '../../components/modules/topbar/topbar';
+import Navbar from '../../components/modules/navbar/navbar';
+import Header from '../../components/templates/header/header';
 
 
 function Home() {
   return (
     <div>
+      <Topbar />
       <Navbar />
-      
+      <Header />
     </div>
   )
 }
