840ea249dc6ad9e227c6c4cc1e47fcb35c8b7db7
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-09-21 23:50:05 +0330

    Routes: import and configure MyAccount page route

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import Home from "./pages/home/home";
 import Shop from "./pages/Shop/Shop"
 import Product from "./pages/Product/Product"
 import ContactUs from "./pages/contact-us/contact-us"
+import Myaccount from "./pages/my-account/my-account";
 
 const routes = [
     { path: '/', element: <Home /> },
     { path: '/Shop', element: <Shop /> },
     { path: '/Product', element: <Product /> },
     { path: '/contact-us', element: <ContactUs /> },
+    { path: '/my-account', element: <Myaccount /> },
 ]
 
 export default routes;
