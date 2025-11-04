d59ceb988050b30ad03e98c1ab25263726105205
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-11-04 12:51:02 +0330

    feat(product-page): add topbar, navbar, breadcrumb, product details, newsletter and footer components

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Topbar from '../../components/modules/topbar/topbar';
+import Navbar from '../../components/modules/navbar/navbar';
+import Newsletter from '../../components/modules/news-letter/news-letter';
+import Footer from '../../components/modules/footer/footer';
+import Breadcrumb from '../../components/modules/breadcrumb/breadcrumb';
+import ProductDetails from '../../components/templates/product-details/product-details';
 
 function Product() {
   return (
     <div>
-      
+      <Topbar />
+      <Navbar />
+      <Breadcrumb />
+      <ProductDetails />
+      <Newsletter />
+      <Footer />
     </div>
   )
 }
