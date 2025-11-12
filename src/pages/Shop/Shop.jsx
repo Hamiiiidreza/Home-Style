d80965960dd25674c62246c380ea02fe6a40ae8e
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-11-12 10:48:25 +0330

    feat: import Header, Breadcrumb, FilterAndSortpanel, ProductSection, Newsletter, and Footer components in Shop page

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import Topbar from '../../components/modules/topbar/topbar';
 import Navbar from '../../components/modules/navbar/navbar';
+import Header from '../../components/templates/header/header';
+import Breadcrumb from '../../components/modules/breadcrumb/breadcrumb';
+import FilterAndSortpanel from '../../components/ui/filter-and-sortpanel';
+import ProductSection from '../../components/modules/product-section/product-section';
+import Newsletter from '../../components/modules/news-letter/news-letter';
+import Footer from '../../components/modules/footer/footer';
 
 function Shop() {
   return (
     <div>
       <Topbar />
       <Navbar />
+      <Header
+        singleImage="/Images/img-placeholder-2.png"
+        isSlidable={false}
+        withGradient={false}
+      />ّ
+      <Breadcrumb />
+      <FilterAndSortpanel />
+      <ProductSection
+        showHeader={false}
+        productCount={12}
+        showBorder={false}
+      />
+      <Newsletter />
+      <Footer />
     </div>
   )
 }
