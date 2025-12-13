92d09dbb2ae6f739495bd4312af8dd6039d419da
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-12-13 14:12:28 +0330

    feat(PageHierarchy): add items prop to make component dynamic

--- src/components/modules/Page-Hierarchy/page-hierarchy.jsx
+++ src/components/modules/Page-Hierarchy/page-hierarchy.jsx
@@ -1,63 +1,37 @@
-import React from 'react'
+import React from "react";
 
-export default function PageHierarchy() {
+export default function PageHierarchy({ items = [] }) {
     return (
-        <div className='container'>
+        <div className="container">
             <nav className="inline-flex items-start gap-4 relative my-4">
                 <ul className="inline-flex items-start gap-4 *:font-InterMedium *:text-sm *:leading-6 *:text-neutral-04">
-                    <li>
-                        <div className="inline-flex items-center gap-1">
-                            <div
-                                className="w-fit"
-                            >
-                                Home
-                            </div>
-                            <img
-                                className="w-3 h-3"
-                                src="/Images/chevron-right.svg"
-                                alt="Stepper separator"
-                            />
-                        </div>
-                    </li>
-                    <li>
-                        <div className="inline-flex items-center gap-1">
-                            <div
-                                className="w-fit"
-                            >
-                                Shop
-                            </div>
-                            <img
-                                className="relative w-3 h-3"
-                                src="/Images/chevron-right.svg"
-                                alt="Stepper separator"
-                            />
-                        </div>
-                    </li>
-                    <li>
-                        <div className="inline-flex items-center gap-1">
-                            <div
-                                className="w-fit"
-                            >
-                                Living Room
-                            </div>
-                            <img
-                                className="relative w-3 h-3"
-                                src="/Images/chevron-right.svg"
-                                alt="Stepper separator"
-                            />
-                        </div>
-                    </li>
-                    <li>
-                        <div className="inline-flex items-center gap-1 text-black">
-                            <div
-                                className="w-fit"
-                            >
-                                Product
-                            </div>
-                        </div>
-                    </li>
+                    {items.map((item, index) => {
+                        const isLast = index === items.length - 1;
+
+                        return (
+                            <li key={index}>
+                                <div
+                                    className={`inline-flex items-center gap-1 transition-all hover:drop-shadow-custom ${isLast ? "text-black" : ""
+                                        }`}
+                                >
+                                    <div className="w-fit">
+                                        {item}
+                                    </div>
+
+                                    {!isLast && (
+                                        <img
+                                            className="w-3 h-3"
+                                            src="/Images/chevron-right.svg"
+                                            alt="Stepper separator"
+                                        />
+                                    )}
+                                </div>
+                            </li>
+                        );
+                    })}
                 </ul>
             </nav>
         </div>
-    )
+    );
 }
+
