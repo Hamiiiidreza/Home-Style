2c8978eb0830543af4d15e9a3754c92feb524e02
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-12-15 18:46:01 +0330

    Add fixed pixel width to SectionHeader component

--- src/components/ui/section-header.jsx
+++ src/components/ui/section-header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const SectionHeader = ({ title, linkText, to, mbClass }) => {
     return (
         <div className={`flex flex-col md:flex-row justify-between items-start md:items-end ${mbClass}`}>
-            <h2 className="font-PoppinsMedium w-[149px] h-auto text-4.5xl text-black leading-none tracking-headline-7 hover:drop-shadow-custom transition-all">
+            <h2 className="font-PoppinsMedium w-[400px] h-auto text-4.5xl text-black leading-none tracking-headline-7 hover:drop-shadow-custom transition-all">
                 {title}
             </h2>
             <Link to={to} className="flex items-center gap-1 border-b border-neutral-07 mt-4 md:mt-0 z-10 transition-all hover:bg-rgba(0,0,0,0.25) hover:shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
