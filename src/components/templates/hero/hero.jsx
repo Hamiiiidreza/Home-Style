ca5fba2754d3c8199e494edc82fb62d9a4da68a3
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-12-21 19:41:15 +0330

    Update hero layout and typography for mobile view

--- src/components/templates/hero/hero.jsx
+++ src/components/templates/hero/hero.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Hero = () => (
     <div className="container">
-        <section className="flex flex-col md:flex-row items-center justify-between gap-8 w-full py-10 mb-10 bg-white">
-            <h2 className="w-[57.5] text-7xl font-PoppinsMedium tracking-hero hover:drop-shadow-custom transition-all">
+        <section className="flex flex-col md:flex-row items-start md:items-center justify-between gap-4 md:gap-8 w-full py-10 mb-0 md:mb-10 bg-white">
+            <h2 className="w-[57.5] text-[40px] md:text-5xl xl:text-7xl font-PoppinsMedium tracking-hero hover:drop-shadow-custom transition-all">
                 <span className="text-neutral-07 inline-block">Simply Unique</span>
                 <span className="text-neutral-04 inline-block">/</span>
                 
