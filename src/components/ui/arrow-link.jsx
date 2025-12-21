87377fff3ed21f9e524a117717988926dce1baa7
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-12-21 19:41:47 +0330

    Hide arrow link component on mobile screens

--- src/components/ui/arrow-link.jsx
+++ src/components/ui/arrow-link.jsx
@@ -5,7 +5,7 @@ import { GoArrowRight } from "react-icons/go";
 function ArrowLink({ title, textColor, borderColor, to }) {
     return (
         <button
-            className={`inline-flex items-center gap-0.5 hover:drop-shadow-custom transition-all border-b ${borderColor}`}
+            className={`hidden sm:inline-flex items-center gap-0.5 hover:drop-shadow-custom transition-all border-b ${borderColor}`}
             aria-label="Shop now for 30% off"
         >
             <div className={`inline-flex items-center ${textColor} gap-1`}>
