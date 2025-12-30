5e38d1d438f877c0f0a832e0f53cc3b2efeca6eb
Hamiiiidreza <ha79mi45dd@gmail.com>
2025-12-30 22:24:32 +0330

    (fix):redesign filter-and-sortpanel component

--- src/components/ui/filter-and-sortpanel.jsx
+++ src/components/ui/filter-and-sortpanel.jsx
@@ -15,6 +15,23 @@ function FilterAndSortpanel({ mode = "shop", defaultActiveButton = 2 }) {
     const [selectedPrice, setSelectedPrice] = useState("All Price");
     const priceDropdownRef = useRef(null);
 
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 767px)");
+
+        const handleMobileActive = () => {
+            if (mediaQuery.matches && activeButton < 3) {
+                setActiveButton(3);
+            }
+        };
+
+        handleMobileActive(); // اجرا در اولین رندر
+        mediaQuery.addEventListener("change", handleMobileActive);
+
+        return () => {
+            mediaQuery.removeEventListener("change", handleMobileActive);
+        };
+    }, [activeButton]);
+
     const buttons = [
         { id: 1, src: "/Images/sort-1.svg", activeSrc: "/Images/sort-1-active.svg", label: "Grid view 1" },
         { id: 2, src: "/Images/sort-2.svg", activeSrc: "/Images/sort-2-active.svg", label: "Grid view 2" },
@@ -63,14 +80,14 @@ function FilterAndSortpanel({ mode = "shop", defaultActiveButton = 2 }) {
 
     return (
         <div>
-            <div className="flex container items-end justify-between w-full">
+            <div className="container w-full flex flex-col gap-6 pt-8 md:flex-row md:flex-wrap md:items-end md:justify-between md:pt-0">
 
                 {/* ----------- MODE: SHOP (Category + Price) ---------- */}
                 {mode === "shop" && (
-                    <div className="w-[548px] items-start gap-6 flex">
+                    <div className="flex flex-col gap-2 w-full md:w-full md:flex-row md:justify-center xl:w-[548px] xl:justify-start">
 
                         {/* ---------- CATEGORIES ---------- */}
-                        <div className="flex flex-col w-[262px] items-start gap-2 relative" ref={catDropdownRef}>
+                        <div className="flex flex-col w-full md:w-[262px] items-start gap-2 relative" ref={catDropdownRef}>
                             <label className="font-InterSemiBold text-neutral-04 text-base leading-[26px] whitespace-nowrap transition-all hover:drop-shadow-custom">
                                 CATEGORIES
                             </label>
@@ -86,36 +103,49 @@ function FilterAndSortpanel({ mode = "shop", defaultActiveButton = 2 }) {
                                 />
                             </button>
 
-                            {isCatOpen && (
-                                <nav
-                                    className="absolute bottom-[110%] left-0 flex flex-col w-[312px] items-start gap-2.5 p-2 bg-white rounded-xl overflow-hidden border-[1.5px] border-solid border-[#f3f5f6] shadow-depth-4 z-50"
-                                    role="navigation"
-                                    aria-label="Category selection menu"
-                                >
-                                    <ul className="w-full" role="list">
-                                        {categories.map((cat) => (
-                                            <li key={cat} className="w-full">
-                                                <button
-                                                    onClick={() => {
-                                                        setSelectedCategory(cat);
-                                                        setIsCatOpen(false);
-                                                    }}
-                                                    className={`flex w-full items-center gap-2 p-2 rounded-lg transition-all ${selectedCategory === cat
-                                                        ? "bg-neutral-02 text-neutral-07 font-InterSemiBold"
-                                                        : "text-neutral-04 hover:bg-neutral-02"
-                                                        }`}
-                                                >
-                                                    <span className="whitespace-nowrap">{cat}</span>
-                                                </button>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </nav>
-                            )}
+
+                            <nav
+                                className={`
+                                            relative w-full mt-2
+                                            flex flex-col gap-2.5 p-2 bg-white rounded-xl
+                                            border-[1.5px] border-[#f3f5f6] shadow-depth-4 z-50
+                                        
+                                            overflow-hidden transition-all duration-300 ease-out
+                                            ${isCatOpen
+                                        ? "max-h-96 opacity-100 translate-y-0"
+                                        : "max-h-0 opacity-0 -translate-y-2 pointer-events-none"
+                                    }
+                                        
+                                            md:absolute md:bottom-[110%] md:left-0 md:w-full
+                                           
+                                          `}
+                                role="navigation"
+                                aria-label="Category selection menu"
+                            >
+                                <ul className="w-full" role="list">
+                                    {categories.map((cat) => (
+                                        <li key={cat} className="w-full">
+                                            <button
+                                                onClick={() => {
+                                                    setSelectedCategory(cat);
+                                                    setIsCatOpen(false);
+                                                }}
+                                                className={`flex w-full items-center gap-2 p-2 rounded-lg transition-all ${selectedCategory === cat
+                                                    ? "bg-neutral-02 text-neutral-07 font-InterSemiBold"
+                                                    : "text-neutral-04 hover:bg-neutral-02"
+                                                    }`}
+                                            >
+                                                <span className="whitespace-nowrap">{cat}</span>
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </nav>
+
                         </div>
 
                         {/* ---------- PRICE ---------- */}
-                        <div className="flex flex-col w-[262px] items-start gap-2 relative" ref={priceDropdownRef}>
+                        <div className="flex flex-col w-full md:w-[262px] items-start gap-2 relative" ref={priceDropdownRef}>
                             <label className="font-InterSemiBold text-neutral-04 text-base leading-[26px] whitespace-nowrap transition-all hover:drop-shadow-custom">
                                 PRICE
                             </label>
@@ -131,32 +161,43 @@ function FilterAndSortpanel({ mode = "shop", defaultActiveButton = 2 }) {
                                 />
                             </button>
 
-                            {isPriceOpen && (
-                                <nav
-                                    className="absolute bottom-[110%] left-0 flex flex-col w-[312px] items-start gap-2.5 p-2 bg-white rounded-xl overflow-hidden border-[1.5px] border-solid border-[#f3f5f6] shadow-depth-4 z-50"
-                                    role="navigation"
-                                    aria-label="Price selection menu"
-                                >
-                                    <ul className="w-full" role="list">
-                                        {prices.map((price) => (
-                                            <li key={price} className="w-full">
-                                                <button
-                                                    onClick={() => {
-                                                        setSelectedPrice(price);
-                                                        setIsPriceOpen(false);
-                                                    }}
-                                                    className={`flex w-full items-center gap-2 p-2 rounded-lg transition-all ${selectedPrice === price
-                                                        ? "bg-neutral-02 text-neutral-07 font-InterSemiBold"
-                                                        : "text-neutral-04 hover:bg-neutral-02"
-                                                        }`}
-                                                >
-                                                    <span className="whitespace-nowrap">{price}</span>
-                                                </button>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </nav>
-                            )}
+                            <nav
+                                className={`
+                                            relative w-full mt-2
+                                            flex flex-col gap-2.5 p-2 bg-white rounded-xl
+                                            border-[1.5px] border-[#f3f5f6] shadow-depth-4 z-50
+                                        
+                                            overflow-hidden transition-all duration-300 ease-out
+                                            ${isPriceOpen
+                                        ? "max-h-96 opacity-100 translate-y-0"
+                                        : "max-h-0 opacity-0 -translate-y-2 pointer-events-none"
+                                    }
+                                        
+                                            md:absolute md:bottom-[110%] md:left-0 md:w-full
+                                            
+  `}
+                                role="navigation"
+                                aria-label="Price selection menu"
+                            >
+                                <ul className="w-full" role="list">
+                                    {prices.map((price) => (
+                                        <li key={price} className="w-full">
+                                            <button
+                                                onClick={() => {
+                                                    setSelectedPrice(price);
+                                                    setIsPriceOpen(false);
+                                                }}
+                                                className={`flex w-full items-center gap-2 p-2 rounded-lg transition-all ${selectedPrice === price
+                                                    ? "bg-neutral-02 text-neutral-07 font-InterSemiBold"
+                                                    : "text-neutral-04 hover:bg-neutral-02"
+                                                    }`}
+                                            >
+                                                <span className="whitespace-nowrap">{price}</span>
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </nav>
                         </div>
                     </div>
                 )}
@@ -187,12 +228,12 @@ function FilterAndSortpanel({ mode = "shop", defaultActiveButton = 2 }) {
                 )}
 
                 {/* ---------- SORT & VIEW BUTTONS ---------- */}
-                <div className="inline-flex items-center gap-8">
+                <div className="flex w-full justify-between items-center md:order-2 md:w-full md:justify-between lg:order-none lg:w-auto lg:justify-normal gap-8">
                     <button
                         className="box-border inline-flex items-center gap-1 border-0 cursor-pointer"
                         aria-label="Sort products"
                     >
-                        <span className="inline-flex items-center gap-1 font-InterSemiBold text-base text-black-900 leading-6.5 whitespace-nowrap transition-all hover:drop-shadow-custom">
+                        <span className="inline-flex items-center gap-1 font-InterSemiBold text-base text-black-900 leading-6.5 z-10 whitespace-nowrap transition-all hover:drop-shadow-custom">
                             Sort by
                         </span>
                         <img
@@ -207,13 +248,15 @@ function FilterAndSortpanel({ mode = "shop", defaultActiveButton = 2 }) {
                         role="group"
                         aria-label="View options"
                     >
-                        {buttons.map((btn) => (
+                        {buttons.map((btn, index) => (
                             <button
                                 key={btn.id}
                                 onClick={() => setActiveButton(btn.id)}
-                                className={`relative w-[46px] h-10 border-r border-solid border-neutral-03 cursor-pointer transition-all ${activeButton === btn.id
-                                    ? "bg-neutral-03"
-                                    : "bg-white hover:bg-neutral-03"
+                                className={`relative w-[46px] h-10 border-r border-solid border-neutral-03 cursor-pointer transition-all
+                                    ${index < 2 ? "hidden md:block" : "block"}
+                                    ${activeButton === btn.id
+                                        ? "bg-neutral-03"
+                                        : "bg-white hover:bg-neutral-03"
                                     }`}
                                 aria-label={btn.label}
                             >
