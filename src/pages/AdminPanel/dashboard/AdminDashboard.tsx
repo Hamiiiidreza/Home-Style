dcde1d8e0892bbc0f3a43e2508c8587631d949ae
Hamiiiidreza <ha79mi45dd@gmail.com>
2026-08-25 20:54:13 +0330

    refactor(admin): restructure dashboard page and modernize routing path

---
+++ src/pages/AdminPanel/dashboard/AdminDashboard.tsx
@@ -0,0 +1,10 @@
+import DashboardScreen from "../../../components/screens/admin-panel/dashboard/dashboard-screen";
+
+const AdminDashboard = () => {
+  return <DashboardScreen />;
+};
+
+export default AdminDashboard;
+
+
+
